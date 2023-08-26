eea2faa1dd96b0502057eadaafc74c57a72e4e46
Sabri ŞEN <sabrisen97@gmail.com>
2023-08-26 15:26:25 +0300

    Navigation properties

--- Hamburger/Hamburger/Models/Entities/OrderDetails.cs
+++ Hamburger/Hamburger/Models/Entities/OrderDetails.cs
@@ -3,11 +3,13 @@
 	public class OrderDetails
 	{
 		public int ID { get; set; }
-		public int OrderID { get; set; }
-		public int ProductID { get; set; }
+		public int? OrderID { get; set; }
+		public int? ProductID { get; set; }
 		public int Quantity { get; set; }
-		public int SizeID { get; set; }
+		public int? SizeID { get; set; }
 		public decimal Price { get; set; }
-		public Order Order { get; set; }
-	}
+		public Order? Order { get; set; }
+		public Size? Size { get; set; }
+        public ICollection<Product>? Products { get; set; }
+    }
 }
