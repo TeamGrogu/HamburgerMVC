eb6dff736b8796503770cd3ecc6afb7d8af94fd1
yolcuo <yolcuuomer@gmail.com>
2023-08-26 10:52:44 +0300

    ömer yolcu

--- Hamburger/Hamburger/Controllers/HomeController.cs
+++ Hamburger/Hamburger/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Diagnostics;
 namespace Hamburger.Controllers
 {
     public class HomeController : Controller
-    {
+    {//2. commitim
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
