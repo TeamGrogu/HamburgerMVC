da76f2cb48f42e4eab29586a37a3db1f844cda36
Mehmet Talha Aksoy <talhaaksoyy@gmail.com>
2023-08-27 10:18:56 +0300

    Last Update(entity)

--- Hamburger/Hamburger/Models/Entities/Menu.cs
+++ Hamburger/Hamburger/Models/Entities/Menu.cs
@@ -4,9 +4,12 @@ namespace Hamburger.Models.Entities
 {
 	public class Menu:BaseEntity
 	{
-        public Product Hamburger { get; set; }
-        public Product Side { get; set; }
-        public Product Drink { get; set; }
+        public int HamburgerID { get; set; }
+		public int SideID { get; set; }
+		public int DrinkID { get; set; }
+		public Product? Hamburger { get; set; }
+        public Product? Side { get; set; }
+        public Product? Drink { get; set; }
         public string? MenuImage { get; set; }
         public decimal Price { get; set; }
         public string? Description { get; set; }
