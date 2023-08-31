67ca576ef2dc0f0eca5a48a6696e769d03996674
Sabri ŞEN <sabrisen97@gmail.com>
2023-08-31 13:28:10 +0300

    a

---
+++ Hamburger/Hamburger/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Hamburger.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hamburger.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult ShoppingCart()
+        {
+            return View();
+        }
+        public IActionResult CreateOrder(int id)
+        {
+            Order order;
+
+            return View();
+        }
+    }
+}
