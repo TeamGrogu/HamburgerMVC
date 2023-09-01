cb33fc785cd69364bcb8d45554db9252dceec756
omerfdev <omerfdev@gmail.com>
2023-09-01 19:28:39 +0300

    Commit Menu CRUD

--- Hamburger/Hamburger/Models/ViewModels/MenuProductVM.cs
+++ Hamburger/Hamburger/Models/ViewModels/MenuProductVM.cs
@@ -6,10 +6,16 @@ namespace Hamburger.Models.ViewModels
     public class MenuProductVM
     {
         public ICollection<SelectListItem> Dropdown { get; set; }
+        public ICollection<SelectListItem> DropdownHamburgers { get; set; }
+        public ICollection<SelectListItem> DropdownBeverages { get; set; }
+        public ICollection<SelectListItem> DropdownSides { get; set; }
         public Product Product { get; set; }
         public Menu Menu { get; set; }
         public ICollection<Product>? Products { get; set; }
         public ICollection<Menu>? Menus { get; set; }
         public ICollection<Category>? Categories { get; set; }
+        public int SelectedHamburgerID { get; set; }
+        public int SelectedSideID { get; set; }
+        public int SelectedBeverageID { get; set; }
     }
 }
