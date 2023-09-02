c252b37f52b958548c3a4e158da64d7fbaa7365c
Sabri ŞEN <sabrisen97@gmail.com>
2023-09-02 20:42:31 +0300

    a

--- Hamburger/Hamburger/Models/ViewModels/LoginVM.cs
+++ Hamburger/Hamburger/Models/ViewModels/LoginVM.cs
@@ -5,14 +5,7 @@ namespace Hamburger.Models.ViewModels
 {
     public class LoginVM
     {
-        [Required]
-        [StringLength(16, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 6)]
-        [SpecialCharacter(ErrorMessage = "The field must contain at least one special character.")]
-        [UpperCase(ErrorMessage = "The field must contain at least one uppercase letter.")]
-        [Number(ErrorMessage = "The field must contain at least one number.")]
         public string Password { get; set; }
-        [Required]
-        [EmailAddress]
         public string Email { get; set; }
     }
 }
