06dc0fc616ae1c4e595cac2fa6fd39181915ff1b
yolcuo <yolcuuomer@gmail.com>
2023-09-02 14:32:14 +0300

    back to list eklendi

---
+++ Hamburger/Hamburger/Validations/EmailCheck.cs
@@ -0,0 +1,28 @@
+﻿using Hamburger.DAL;
+using Hamburger.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hamburger.Validations
+{
+	public class EmailCheck:ValidationAttribute
+	{
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+		{
+			if (value == null)
+				return ValidationResult.Success; // Let the Required attribute handle null values
+
+			string email = value.ToString();
+			var userService = (IUserService)validationContext.GetService(typeof(IUserService)); // Replace with your user service interface
+
+			if (userService.IsEmailInUse(email))
+			{
+				return new ValidationResult(ErrorMessage ?? "Email is already in use.");
+			}
+
+			return ValidationResult.Success;
+		}
+	}
+}
